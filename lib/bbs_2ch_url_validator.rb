33fe0d0b78b0ea2d0686716851c5d1dd0aa30624
dogwood008 <dogwood008+github@gmail.com>
2016-08-02 09:51:01 +0900

    :recycle: Refactoring

--- lib/bbs_2ch_url_validator.rb
+++ lib/bbs_2ch_url_validator.rb
@@ -3,30 +3,43 @@ require 'uri'
 
 module Bbs2chUrlValidator
   # http://www.rubular.com/r/UEVtzBwbtM
-  VALID_2CH_URL_REGEX = %r(^http:\/\/((?<server_name>.+)\.)?(?<open_flag>open)?2ch.(?<tld>net|sc)\/?((test\/read\.cgi\/)?(?<board_name>\w+?)\/?((?<thread_key>\d{10,11})\/?)?)?((?<dat_flag>dat)\/(?<thread_key2>\d{10,11})\.dat)?((?<subject_flag>subject)\.txt)?((?<setting_flag>SETTING)\.TXT)?$)
+  VALID_2CH_URL_REGEX = %r(^http:\/\/((?<server_name>.+)\.)?(?<is_open>open)?2ch.(?<tld>net|sc)\/?((test\/read\.cgi\/)?(?<board_name>\w+?)\/?((?<thread_key>\d{9,10})\/?)?)?((?<is_dat>dat)\/(?<thread_key2>\d{9,10})\.dat)?((?<is_subject>subject)\.txt)?((?<is_setting>SETTING)\.TXT)?$)
 
   class URL
     def self.parse(url)
       URI.parse(url)
-      group_names = VALID_2CH_URL_REGEX.named_captures.to_a.sort {|a,b| a[1] <=> b[1] }.map {|n| n[0]}
       VALID_2CH_URL_REGEX.match(url) do |m|
-        hash = {}
-        group_names.each_with_index do |n, i|
-          hash[n] = m[i+1]
-        end
-        hash['thread_key'] = hash['thread_key'] || hash['thread_key2']
-        hash.delete('thread_key2')
+        hash = generate_group_name_value_pairs(extract_and_sort_group_names, m)
+        hash = remove_duplicate_key(hash)
         UrlInfo.new(hash)
       end
+    end
+
+    def self.extract_and_sort_group_names
+      VALID_2CH_URL_REGEX.named_captures.to_a.sort { |a, b| a[1] <=> b[1] }.map { |n| n[0] }
+    end
+
+    def self.generate_group_name_value_pairs(group_names, matchdata)
+      hash = {}
+      group_names.each_with_index do |n, i|
+        hash[n] = matchdata[i + 1]
+      end
+      hash
+    end
+
+    def self.remove_duplicate_key(hash)
+      hash['thread_key'] = hash['thread_key'] || hash['thread_key2']
+      hash.delete('thread_key2')
+      hash
     end
   end
 
   class UrlInfo
-    attr_reader :server_name, :tld, :board_name, :thread_key, :open_flag, \
-      :dat_flag, :subject_flag, :setting_flag
+    attr_reader :server_name, :tld, :board_name, :thread_key, :is_open, \
+      :is_dat, :is_subject, :is_setting
     def initialize(hash)
       hash.each do |k, v|
-        value = k.end_with?('flag') ? !v.nil? : v
+        value = k.start_with?('is_') ? !v.nil? : v
         instance_variable_set("@#{k}", value)
       end
     end
